4a9ef1af15cfdac20ae96bf0ce2cb8ce4c3b01c4
Rhett-wang-888 <120445143@qq.com>
2021-02-14 14:05:10 +0800

    test

---
+++ algo/LengthOfLongestSubstring.py
@@ -0,0 +1,22 @@
+# @title: LengthOfLongestSubstring
+# @description: 无重复字符的最长字符串
+# @author  Rhett
+# @date 2021/2/14 14:00
+
+class LengthOfLongestSubstring:
+    def LengthOfLongestSubstring(self,s:str)->int:
+        if not s:return 0
+        left=0
+        lookup=set()
+        n=len(s)
+        max_len=0
+        cur_len=0
+        for i in range(n):
+            cur_len +=1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left +=1
+                cur_len -=1
+            if cur_len>max_len:max_len=cur_len
+            lookup.add(s[i])
+        return max_len
