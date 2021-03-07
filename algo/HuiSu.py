1ed1e1036af28a7105ab897332ec6d2e61dff3c4
Rhett-wang-888 <120445143@qq.com>
2021-03-07 14:40:11 +0800

    test

---
+++ algo/HuiSu.py
@@ -0,0 +1,28 @@
+# @title: HuiSu
+# @description: TODO 字符串回文分割 使用回溯方法
+# @author  Rhett
+# @date 2021/3/7 14:28
+from typing import List
+
+
+class Solution:
+    def partition(self,s:str)->List[List[str]]:
+        n =len(s)
+        f=[[True]*n for _ in range(n)]
+
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                f[i][j]=(s[i]==s[j]) and f[i+1][j-1]
+        ret =list()
+        ans=list()
+        def dfs(i:int):
+            if i==n:
+                ret.append(ans[:])
+                return
+            for j in range(j,n):
+                if f[i][j]:
+                    ans.append(s[i:j+1])
+                    dfs(j+1)
+                    ans.pop()
+        dfs(0)
+        return ret
