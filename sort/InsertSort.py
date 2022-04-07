362674bf32bf45a87d9f1ab1959ec960f3752e7b
Rhett-wang-888 <120445143@qq.com>
2022-04-07 10:30:21 +0800

    test

--- sort/InsertSort.py
+++ sort/InsertSort.py
@@ -16,6 +16,32 @@ class InsertSort:
                 j-=1
             nums[j]=tmp
         return nums
+    def sortArray1(self,nums:List[int])->List[int]:
+        n = len(nums)
+        for i in range(1,n):
+            tmp=nums[i]
+            j=i
+            while j>0 and nums[j-1]>tmp :
+                nums[j]=nums[j-1]
+                j -= 1
+            nums[j]=tmp
+
+        return nums
+    def sortArray2(self,data):
+        for i in range(1,len(data)):
+            value= data[i]
+            j=i-1
+
+            while j>=0:
+                if data[j]>value:
+                    data[j+1]=data[j]
+                else :
+                    break
+                j-=1
+            data[j+1]=value
+        return data
+
+
 if __name__=='__main__':
     a=[20,10,30,50,40,60]
     b=InsertSort().sortArray(a)
---
+++ sort/__init__.py
@@ -0,0 +1,5 @@
+# @title: __init__.py
+# @description: TODO
+# @author  Rhett
+# @date 2021/10/18 11:27
+    
